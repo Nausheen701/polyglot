abdb8f9c3513b403c5043ee340c462db541b7064
Nausheen Akhter <nausheen701@gmail.com>
2022-04-30 19:18:54 +0300

    added navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,13 @@ import { NavLink } from "react-router-dom"
 const NavBar = () => {
     return (
       <div className="navbar">
-          
+        <NavLink to="/">Home </NavLink> <br></br> 
+        <NavLink to="/about">About this project  </NavLink><br></br>
+        <NavLink to="/species">Species List </NavLink><br></br><br></br>  
+        <NavLink to="/form"> Add an Endangered Species </NavLink><br></br>
+        <NavLink to="/organizations">  Organizations </NavLink><br></br><br></br>  
+        <NavLink to="/orgform"> Add an Organization </NavLink><br></br>
+      </div>
 
       </div>
     )
